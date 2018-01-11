9b2f0c86880a8eb524ac2de0b290e41e41d8abb8
Philipe Casarotte <philipetauk@gmail.com>
2018-01-11 20:07:13 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+		return (
+			<address>Santa Monica, CA</address>
+		)
+	}
+}
+//
+// class SimpleSlider extends Component {
+// 	render() {
+// 		var settings = {
+// 			dots: true,
+// 			infinite: true,
+// 			speed: 500,
+// 			slidesToShow: 1,
+// 			slidesToScroll: 1,
+// 			centerMode: 1
+// 		};
+// 		return (
+// 			<Slider {...settings}>
+// 				<div><a href="/project/dburns"><img src="%PUBLIC_URL%/images/dburns-logo.jpg" width="auto" height="auto" alt="DBurns Logo" Title="DBurns Logo"/></a></div>
+// 				<div><a href="/project/fighter-diet"><img src="%PUBLIC_URL%/images/fighter-diet-logo.jpg" width="auto" height="auto" alt="Fighter Diet Logo" Title="Fighter Diet Logo"/></a></div>
+// 				<div><a href="/project/nikki-sharp"><img src="%PUBLIC_URL%/images/nikki-sharp-logo.jpg" width="auto" height="auto" alt="Nikki Sharp Logo" Title="Nikki Sharp Logo"/></a></div>
+// 				<div><a href="/project/insedia"><img src="%PUBLIC_URL%/images/insedia-logo.jpg" width="auto" height="auto" alt="Insedia Logo" Title="Insedia Logo"/></a></div>
+// 				<div><a href="/project/alter-agents"><img src="%PUBLIC_URL%/images/alter-agents-logo.jpg" width="auto" height="auto" alt="Alter Agents Logo" Title="Alter Agents Logo"/></a></div>
+// 				<div><a href="/project/zen-arts"><img src="%PUBLIC_URL%/images/zen-arts-la-logo.jpg" width="auto" height="auto" alt="Zen Arts La Logo" Title="Zen Arts La Logo"/></a></div>
+// 			</Slider>
+// 		);
+// 	}
+// }
+
+export default App;
